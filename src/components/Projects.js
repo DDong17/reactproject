2aca7cd654236e3be277d1069660921e4957c2ac
DongHo Hwang <ghkdehdgh11@gmail.com>
2022-12-22 17:03:13 +0900

    이미지 카드

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 
-
+//이미지 카드
 export const Projects = () => {
     const projects = [
         {
